16231b678ca1f724fcb4da2d8c9d5351698abdb1
xubo961 <xubop961@gmail.com>
2025-02-11 09:38:48 +0100

    Primer commit :)

---
+++ src/main/java/backend/niamniamapp/NiamniamappApplication.java
@@ -0,0 +1,13 @@
+package backend.niamniamapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiamniamappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiamniamappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/niamniamapp/NiamniamappApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.niamniamapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiamniamappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
