f5ef67bf80a91fca36a08e7ba5d0b8ad6afb68ad
xubo961 <xubop961@gmail.com>
2025-02-26 14:12:50 +0100

    Añadido nuevas cosas e implementado nuevos metodos y mejorado la parte de Favoritos

--- src/main/java/backend/niamniamapp/repositories/FavoritosRecetaRepository.java
+++ src/main/java/backend/niamniamapp/repositories/FavoritosRecetaRepository.java
@@ -1,15 +1,12 @@
 package backend.niamniamapp.repositories;
 
 import backend.niamniamapp.models.FavoritosReceta;
-import backend.niamniamapp.models.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface FavoritosRecetaRepository extends JpaRepository<FavoritosReceta, Long> {
-
-    List<FavoritosReceta> findByListUserContaining(Users user);
-
+    Optional<FavoritosReceta> findByIdReceta(Long idReceta);
 }
